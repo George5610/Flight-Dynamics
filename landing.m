40758cb2f7d7dd49587e71f3fecf2938fe39f274
George Johnson <georgejohnson5610@gmail.com>
2021-03-14 17:29:51 +0000

    Add files via upload

--- landing.m
+++ landing.m
@@ -1,3 +1,4 @@
+
 function [Sa_land, S_land, G_run, V_stall, V_app, V_tran, V_land] = landing(W)
 %% Variables
 p = 101325; %pressure in pascals
@@ -40,5 +41,76 @@ S_land = (D_eff / L) * (50 + (V_app^2 - V_land^2) / 2 * g);
 
 %% Ground Run
 G_run = V_land^2 / (2 * mu * g);
+
+%% Graphing landing Approximation
+
+figure(1)
+clf
+App_Vx = [-290, 0]; %approach x
+App_Vy = [15.24, 0]; %approach y
+rwx = [0 750]; %runway x
+rwy = [-2 -2]; %runway x
+L_point = [0 0]; %landing point
+L_approx = [Sa_land 0]; %landing distance approx
+txt1 = 'Landing approximation with 3 degree aproach vector \newline starting at 15.24m';
+
+hold on;
+p = plot(rwx, rwy, App_Vx, App_Vy, '--b', L_approx, L_point, 'red');
+text(0,100,txt1,'HorizontalAlignment','left');
+xlim([-290 750]);
+ylim([-10 250]);
+p(1).LineWidth = 3;
+p(1).Color = 'black';
+p(3).LineWidth = 2;
+title('Landing Approximation');
+xlabel('Distance (Meters)');
+ylabel('Altitude (Meters)');
+hold off
+
+%% Graphing landing
+figure(2)
+clf
+App_Vx = [-290, 0]; %approach x
+App_Vy = [15.24, 0]; %approach y
+rwx = [0 750]; %runway x
+rwy = [-2 -2]; %runway x
+L_point = [0 0]; %landing point
+L_dis = [S_land 0]; %landing distance approx
+txt2 = 'Landing calculation with 3 degree aproach vector \newline starting at 15.24m';
+
+hold on
+p = plot(rwx, rwy, App_Vx, App_Vy, '--b', L_dis, L_point, 'red');
+text(0,100,txt2,'HorizontalAlignment','left');
+xlim([-290 750]);
+ylim([-10 250]);
+p(1).LineWidth = 3;
+p(1).Color = 'black';
+p(3).LineWidth = 2;
+title('Landing, Accurate');
+xlabel('Distance (Meters)');
+ylabel('Altitude (Meters)');
+hold off
+
+%% Graphing ground run
+figure(3)
+clf
+rwx = [0 750]; %runway x
+rwy = [-2 -2]; %runway x
+L_point = [0 0]; %landing point
+L_gr = [G_run 0]; %landing distance approx
+txt3 = 'Ground run calculation';
+
+hold on
+p = plot(rwx, rwy, App_Vx, App_Vy, '--b', L_gr, L_point, 'red');
+text(0,100,txt3,'HorizontalAlignment','left');
+xlim([-290 750]);
+ylim([-10 250]);
+p(1).LineWidth = 3;
+p(1).Color = 'black';
+p(3).LineWidth = 2;
+title('Ground Run Distance');
+xlabel('Distance (Meters)');
+ylabel('Altitude (Meters)');
+hold off
 
 end
